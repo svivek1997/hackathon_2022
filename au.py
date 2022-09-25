4e7abf1a50c799c58bcf4872e06a64398b12c837
svivek1997 <47782502+svivek1997@users.noreply.github.com>
2022-09-24 21:22:32 -0500

    Update au.py

--- au.py
+++ au.py
@@ -1,7 +1,7 @@
 import serial
 from pymongo import MongoClient
 import uuid
-import time
+from datetime import datetime
 import urllib
 
 CONNECTION_STRING = "mongodb+srv://vsaraff:"+urllib.parse.quote("??????")+"@cluster0.timjy8c.mongodb.net/test"
@@ -12,30 +12,27 @@ client = MongoClient(CONNECTION_STRING)
 # Create the database for our example (we will use the same database throughout the tutorial
 db = client['aurdino']['stats']
 data_db = {}
+
 with serial.Serial('COM3', 9600, timeout=1) as ser:
     while True:
         line = str(ser.readline())
         if len(line) > 55:
             line = line[2:-5]
-            print(line)
             myuuid = str(uuid.uuid4())
-            ts = str(time.time())
+            # ts = str(time.time())
+            now = datetime.now()
+            date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
             linefields = line.split(",")
             field1 = linefields[0].split(":")
             field2 = linefields[1].split(":")
             field3 = linefields[2].split(":")
             data_db["ID"] = myuuid
             data_db[field1[0]] = field1[1]
-            data_db[field2[0]] = field2[1]
-            data_db[field3[0]] = field3[1]
-            data_db["TimeStamp"] = ts
+            data_db[field2[0]] = float(field2[1].strip())
+            data_db[field3[0]] = float(field3[1].strip())
+            data_db["TimeStamp"] = date_time
             print(data_db)
             db.insert_one(data_db)
             data_db = {}
 
 
-    # x = ser.read()          # read one byte
-    # s = ser.read(10)        # read up to ten bytes (timeout)
-    # line = ser.readline()
-
-    # db_values = {}
